be3ad29e2c8611111540976fcf69c4246257c26e
sivishashankar <sivishashankar@gmail.com>
2017-01-04 02:05:05 +0530

    Update timetable.js

--- sample/timetable.js
+++ sample/timetable.js
@@ -33,8 +33,8 @@
  }
  */
 var app = angular.module("myapp", []);
-app.controller("ListController", ['$scope',
-function($scope) {
+app.controller("ListController", ['$scope','$http',
+function($scope, $http) {
 	$scope.reviewIndex = 0;
 	$scope.reviewDay = "satR";
 	$scope.reviewNotes = "";
@@ -44,30 +44,82 @@ function($scope) {
 	$scope.heads = []; 
 	$scope.rowTotal = [];
 	$scope.hideReviewPopup = true;
+	$scope.rowTotal = ['00:00', '00:00', '00:00', '00:00', '00:00', '00:00', '00:00'];
+	
 	$scope.add = function() {
-		$scope.timetables.push({
-			'id' : 0,
-			'modified' : 1,
-			'client' : '',
-			'project' : '',
-			'sat' : 0,
-			'satR' : '',
-			'sun' : 0,
-			'sunR' : '',
-			'mon' : 0,
-			'monR' : '',
-			'tue' : 0,
-			'tueR' : '',
-			'wed' : 0,
-			'wedR' : '',
-			'thu' : 0,
-			'thuR' : '',
-			'fri' : 0,
-			'friR' : ''
-		});
+		$scope.addTimetablejsontoTimesheetArray();
 	};
 	
-	$scope.rowTotal = [0, 0, 0, 0, 0, 0, 0];
+	$scope.addTimetablejsontoTimesheetArray = function(){
+		var timesheet = {
+			'modified' : 1,
+			'client' :{'id':0,'name':''},
+			'project' : {'id':0,'name':''},
+			'location' : '',
+			'category':'',
+			'sat' : {'id':0,'time':'00:00','remark':''},
+			'sun' : {'id':0,'time':'00:00','remark':''},
+			'mon' : {'id':0,'time':'00:00','remark':''},
+			'tue' : {'id':0,'time':'00:00','remark':''},
+			'wed' : {'id':0,'time':'00:00','remark':''},
+			'thu' : {'id':0,'time':'00:00','remark':''},
+			'fri' : {'id':0,'time':'00:00','remark':''}
+		};
+		$scope.timetables.push(timesheet);
+	};
+	var timeCalculation = function(time1, time2){
+		var totalMin = parseInt(time1.split(':')[1])
+						+parseInt(time2.split(':')[1]);
+		console.log(totalMin);
+		var min = totalMin % 60;
+		var hour = parseInt(time1.split(':')[0])
+					+parseInt(time2.split(':')[0]);
+		return (hour<10?('0'+hour):hour)+":"+(min<10?('0'+min):min) ;
+	};
+	
+	$scope.calculateTimePerWeek =function(sun, mon, tue, wed, thu, fri, sat){
+		return timeCalculation(
+				timeCalculation(
+					timeCalculation(
+					timeCalculation(
+						timeCalculation(
+							timeCalculation(sun, mon)
+							,tue)
+							,wed)
+							,thu)
+							,fri)
+							,sat);
+	};
+	$scope.createInitialBean = function(jsonDatafromserver){
+		var days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fir', 'sat'];
+		angular.forEach(beanDatafromserver, function(jsonData) { 
+		  var flag = false;
+		  var datefromserver = jsonData.TaskDate.split(' ')[0].split('-');
+		  var taskDate  = new Date(paseInt(datefromserver[0]),(datefromserver(jsonDate[1])-1),datefromserver(jsonDate[2]));
+		  angular.forEach($scope.timetables, function(time){
+		  	if(time.client.name==jsonData.GOCName && time.project.name==jsonData.ProjectName ){
+		  		var day = time[days[taskDate.getDay()]];
+		  		if(day.id==0){
+			  		day.id = jsonData.uid;
+			  		day.remark = josnData.Remarks;
+			  		day.time = jsonData.Task_Hrs+":"+jsonData.Task_Mins;
+			  		flag = true;
+			  	}
+		  	}
+		  	
+		  	if(flag) return;
+		  });
+		  if(!flag){
+		  	var time = $scope.addTimetablejsontoTimesheetArray();
+		  	var day = time[days[taskDate.getDay()]];
+		  	day.id = jsonData.uid;
+			day.remark = josnData.Remarks;
+			day.time = jsonData.Task_Hrs+":"+jsonData.Task_Mins;
+		  }
+		});
+
+	};
+	
 	// previous week navigation button function
 	$scope.prev = function() {
 		$scope.timetables = [];//clearing the existing table
@@ -110,16 +162,16 @@ function($scope) {
 	};
 	
 	$scope.udpatePerDayWork = function(index, day) {
-		$scope.rowTotal = [0, 0, 0, 0, 0, 0, 0];
+		$scope.rowTotal = ['00:00', '00:00', '00:00', '00:00', '00:00', '00:00', '00:00'];
 		$scope.timetables[index].modified=2;
 		for ( i = 0; i < $scope.timetables.length; i++) {
-			$scope.rowTotal[0] += $scope.timetables[i].sat;
-			$scope.rowTotal[1] += $scope.timetables[i].sun;
-			$scope.rowTotal[2] += $scope.timetables[i].mon;
-			$scope.rowTotal[3] += $scope.timetables[i].tue;
-			$scope.rowTotal[4] += $scope.timetables[i].wed;
-			$scope.rowTotal[5] += $scope.timetables[i].thu;
-			$scope.rowTotal[6] += $scope.timetables[i].fri;
+			$scope.rowTotal[0] =timeCalculation($scope.rowTotal[0], $scope.timetables[i].sat.time);
+			$scope.rowTotal[1] =timeCalculation($scope.rowTotal[1], $scope.timetables[i].sun.time);
+			$scope.rowTotal[2] =timeCalculation($scope.rowTotal[2], $scope.timetables[i].mon.time);
+			$scope.rowTotal[3] =timeCalculation($scope.rowTotal[3], $scope.timetables[i].tue.time);
+			$scope.rowTotal[4] =timeCalculation($scope.rowTotal[4], $scope.timetables[i].wed.time);
+			$scope.rowTotal[5] =timeCalculation($scope.rowTotal[5], $scope.timetables[i].thu.time);
+			$scope.rowTotal[6] =timeCalculation($scope.rowTotal[6], $scope.timetables[i].fri.time);
 		}
 	};
 	
@@ -153,7 +205,19 @@ function($scope) {
 		);
 		
 	};
-	
+	$scope.calculateTotalProject = function(){
+		return timeCalculation(
+				timeCalculation(
+					timeCalculation(
+					timeCalculation(
+						timeCalculation(
+							timeCalculation($scope.rowTotal[0], $scope.rowTotal[1])
+							,$scope.rowTotal[2])
+							,$scope.rowTotal[3])
+							,$scope.rowTotal[4])
+							,$scope.rowTotal[5])
+							,$scope.rowTotal[6]);
+	};
 	$scope.setTimedate();
 	$scope.dayHead();
 }]);
